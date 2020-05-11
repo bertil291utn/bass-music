248c9d2eb406c3cedafbf07f7b81a7808ed76fc9
bertil291utn <bertilt4@gmail.com>
2020-05-10 22:40:03 -0500

    Display tracks

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,9 @@ class AlbumsController < ApplicationController
   def index
     @albums = Album.all
   end
+
+  def show
+    @album = Album.find(params[:id])
+    @tracks = @album.tracks
+  end
 end
