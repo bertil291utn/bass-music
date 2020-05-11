1f9081f5f00b473beb405e8b479ce4103502ce68
bertil291utn <bertilt4@gmail.com>
2020-05-10 22:33:39 -0500

    FIx objects model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_014700) do
+ActiveRecord::Schema.define(version: 2020_05_11_025214) do
 
   create_table "albums", force: :cascade do |t|
     t.string "cover"
