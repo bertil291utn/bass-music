408c5d122e398ff4e67494547972a9c6632d727a
bertil291utn <bertilt4@gmail.com>
2020-05-10 20:54:50 -0500

    Add table fields

--- db/migrate/20200511012356_create_tracks.rb
+++ db/migrate/20200511012356_create_tracks.rb
@@ -1,7 +1,9 @@
 class CreateTracks < ActiveRecord::Migration[5.2]
   def change
     create_table :tracks do |t|
-
+      t.string :name
+      t.string :minutes
+      t.references :album
       t.timestamps
     end
   end
--- db/migrate/20200511014700_create_albums.rb
+++ db/migrate/20200511014700_create_albums.rb
@@ -1,7 +1,9 @@
 class CreateAlbums < ActiveRecord::Migration[5.2]
   def change
     create_table :albums do |t|
-
+      t.string :cover
+      t.string :title
+      t.string :artist
       t.timestamps
     end
   end
